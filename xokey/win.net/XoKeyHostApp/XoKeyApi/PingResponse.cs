ed331db88baa40f884a39d81e769abd2123beba8
Karl Hiramoto <karl@hiramoto.org>
2013-09-24 15:54:52 +0200

    data types that will be passed back over JSON API.

---
+++ xokey/win.net/XoKeyHostApp/XoKeyApi/PingResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace XoKeyApi
+{
+    public class PingResponse
+    {
+ 
+        [DataMember(Name = "client_ip")]
+        public string client_ip { get; set; }
+        [DataMember(Name = "ack")]
+        public MsgAck ack;
+    }
+}
