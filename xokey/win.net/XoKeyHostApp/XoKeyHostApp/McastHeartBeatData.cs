7234b27318ffdf10bc36f131d66b627a91dfc021
Karl Hiramoto <karl@hiramoto.org>
2013-10-22 12:51:00 +0200

    data structure of what is sent over UDP multicast

---
+++ xokey/win.net/XoKeyHostApp/XoKeyHostApp/McastHeartBeatData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace XoKeyHostApp
+{
+    [StructLayout(LayoutKind.Explicit, Size = 56, Pack = 1)]
+    public struct McastHeartBeatData
+    {
+        [MarshalAs(UnmanagedType.U4)]
+        [FieldOffset(0)]
+        public uint Version;
+
+        [MarshalAs(UnmanagedType.U4)]
+        [FieldOffset(4)]
+        public uint Magic;
+
+        [MarshalAs(UnmanagedType.U4)]
+        [FieldOffset(8)]
+        public uint IP_Address;
+
+        [MarshalAs(UnmanagedType.U4)]
+        [FieldOffset(12)]
+        public uint Addr_Prefix;
+    }
+}
