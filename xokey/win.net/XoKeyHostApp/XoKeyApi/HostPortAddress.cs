2647e5b6218cc7cf64976dff38a2bf962b9a8999
Karl Hiramoto <karl@hiramoto.org>
2013-10-24 17:22:03 +0200

    VPN key and host address objects used to get VPN state.

---
+++ xokey/win.net/XoKeyHostApp/XoKeyApi/HostPortAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace XoKeyApi
+{
+    public class HostPortAddress
+    {
+        [DataMember(Name = "host")]
+        public string host { get; set; }
+        [DataMember(Name = "port")]
+        public int port { get; set; }
+    }
+}
---
+++ xokey/win.net/XoKeyHostApp/XoKeyApi/VpnKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace XoKeyApi
+{
+    public class VpnKey
+    {
+        [DataMember(Name = "path")]
+        public string path { get; set; }
+
+        [DataMember(Name = "id")]
+        public int id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string name { get; set; }
+
+        [DataMember(Name = "last_use")]
+        public string last_use { get; set; }
+
+        [DataMember(Name = "state")]
+        public string state { get; set; }
+
+        [DataMember(Name = "address")]
+        public HostPortAddress[] address { get; set; }
+    }
+}
