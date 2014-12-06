5ab3d1163b3a63a85a0381c51b48cd11180ee3c9
Karl Hiramoto <karl@hiramoto.org>
2014-12-06 20:38:53 +0100

    add notification window

---
+++ xokey/win.net/EK_App/NotificationWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace EK_App
+{
+    /// <summary>
+    /// Interaction logic for NotificationWindow.xaml
+    /// </summary>
+    public partial class NotificationWindow : Window, INotifyPropertyChanged
+    {
+
+        private string __Status_Message;
+        // Declare the event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Status_Message
+        {
+            get { return __Status_Message; }
+            set
+            {
+                __Status_Message = value;
+                // Call OnPropertyChanged whenever the property is updated
+
+                OnPropertyChanged("Status_Message");
+            }
+        }
+  
+        
+        public NotificationWindow()
+            : base()
+        {
+            this.InitializeComponent();
+            this.Closed += this.NotificationWindowClosed;
+        }   
+      
+
+        public new void Show(string msg)
+        {
+            this.Topmost = true;
+            Status_Message = msg;
+            base.Show();
+
+            this.Owner = System.Windows.Application.Current.MainWindow;
+            this.Closed += this.NotificationWindowClosed;
+            var workingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
+
+            this.Left = workingArea.Right - this.ActualWidth;
+            double top = workingArea.Bottom - this.ActualHeight;
+
+            foreach (Window window in System.Windows.Application.Current.Windows)
+            {
+                string windowName = window.GetType().Name;
+
+                if (windowName.Equals("NotificationWindow") && window != this)
+                {
+                    window.Topmost = true;
+                    top = window.Top - window.ActualHeight;
+                }
+            }
+
+            this.Top = top;
+            this.Status_Message = msg;
+            this.DataContext = msg;
+        }
+
+          // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        private void ImageMouseUp(object sender,
+            System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DoubleAnimationCompleted(object sender, EventArgs e)
+        {
+            if (!this.IsMouseOver)
+            {
+                this.Close();
+            }
+        }
+        private void NotificationWindowClosed(object sender, EventArgs e)
+        {
+            foreach (Window window in System.Windows.Application.Current.Windows)
+            {
+                string windowName = window.GetType().Name;
+
+                if (windowName.Equals("NotificationWindow") && window != this)
+                {
+                    // Adjust any windows that were above this one to drop down
+                    if (window.Top < this.Top)
+                    {
+                        window.Top = window.Top + this.ActualHeight;
+                    }
+                }
+            }
+        }
+    }
+}
