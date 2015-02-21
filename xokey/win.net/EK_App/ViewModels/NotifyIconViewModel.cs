934e8b1dfb2a34184ed2479951c6ebf9ebf5e6d5
Karl Hiramoto <karl@hiramoto.org>
2015-02-21 21:04:07 +0100

    add enable and disable startup.
    change visibility on window

--- xokey/win.net/EK_App/ViewModels/NotifyIconViewModel.cs
+++ xokey/win.net/EK_App/ViewModels/NotifyIconViewModel.cs
@@ -2,16 +2,90 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
+using System.Diagnostics;
+using System.ComponentModel;
+
 
 namespace EK_App.NotifyIconViewModels
 {
     /// <summary>
     /// Provides bindable properties and commands for the NotifyIcon. In this sample, the
     /// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
-    /// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
-    /// </summary>
-    public class NotifyIconViewModel
+    /// in App.xaml.cs could have created this view model,
+    public class NotifyIconViewModel : System.ComponentModel.INotifyPropertyChanged 
     {
+        System.ComponentModel.BackgroundWorker CreateStartupWorker = null;
+        System.ComponentModel.BackgroundWorker DeleteStartupWorker = null;
+
+        // This event handler is where the time-consuming work is done. 
+        private void Enable_Startup_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            // Start the child process.
+            Process enable_proc = new Process();
+            // Redirect the output stream of the child process.
+            enable_proc.StartInfo.UseShellExecute = false;
+            enable_proc.StartInfo.RedirectStandardOutput = true;
+            enable_proc.StartInfo.FileName = "schtasks";
+            enable_proc.StartInfo.Arguments = "/create /sc onlogon /F /tn ExoKey /rl highest /tr \"\\\"" +
+                System.Reflection.Assembly.GetExecutingAssembly().Location  + "\\\"\" ";
+
+            enable_proc.Start();
+            // Do not wait for the child process to exit befor
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string output = enable_proc.StandardOutput.ReadToEnd();
+            enable_proc.WaitForExit();
+            System.Console.WriteLine("schtasks" + output);
+        }
+
+        private void Disable_Startup_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            // Start the child process.
+            Process enable_proc = new Process();
+            // Redirect the output stream of the child process.
+            enable_proc.StartInfo.UseShellExecute = false;
+            enable_proc.StartInfo.RedirectStandardOutput = true;
+            enable_proc.StartInfo.FileName = "schtasks";
+            enable_proc.StartInfo.Arguments = "/delete /F /TN ExoKey";
+
+            enable_proc.Start();
+            // Do not wait for the child process to exit befor
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string output = enable_proc.StandardOutput.ReadToEnd();
+            enable_proc.WaitForExit();
+            System.Console.WriteLine("schtasks" + output);
+        }
+        public void Enable_Startup()
+        {
+            if (CreateStartupWorker == null)
+            {
+                CreateStartupWorker = new System.ComponentModel.BackgroundWorker();
+                CreateStartupWorker.DoWork += Enable_Startup_DoWork;
+            }
+
+            if (CreateStartupWorker.IsBusy)
+                return;
+
+            CreateStartupWorker.RunWorkerAsync();
+        }
+
+        public void Disable_Startup()
+        {
+            if (DeleteStartupWorker == null)
+            {
+                DeleteStartupWorker = new System.ComponentModel.BackgroundWorker();
+                DeleteStartupWorker.DoWork += Disable_Startup_DoWork;
+            }
+
+            if (CreateStartupWorker.IsBusy)
+                return;
+
+            DeleteStartupWorker.RunWorkerAsync();
+        }
+
         /// <summary>
         /// Shows a window, if none is already open.
         /// </summary>
@@ -21,6 +95,15 @@ namespace EK_App.NotifyIconViewModels
             {
                 return new DelegateCommand
                 {
+                    CanExecuteFunc = () => Application.Current.MainWindow.Visibility != Visibility.Visible,
+                    CommandAction = () =>
+                    {
+                        Application.Current.MainWindow.Visibility = Visibility.Visible;
+                    }
+                };
+                /*
+                return new DelegateCommand
+                {
                     CanExecuteFunc = () => Application.Current.MainWindow == null,
                     CommandAction = () =>
                     {
@@ -28,6 +111,7 @@ namespace EK_App.NotifyIconViewModels
                         Application.Current.MainWindow.Show();
                     }
                 };
+                 * */
             }
         }
 
@@ -40,9 +124,16 @@ namespace EK_App.NotifyIconViewModels
             {
                 return new DelegateCommand
                 {
+                    CommandAction = () => Application.Current.MainWindow.Visibility = Visibility.Hidden,
+                    CanExecuteFunc = () => Application.Current.MainWindow.Visibility == Visibility.Visible
+                };
+                /*
+                return new DelegateCommand
+                {
                     CommandAction = () => Application.Current.MainWindow.Close(),
                     CanExecuteFunc = () => Application.Current.MainWindow != null
                 };
+                 */
             }
         }
 
@@ -57,6 +148,43 @@ namespace EK_App.NotifyIconViewModels
                 return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
             }
         }
+
+        /// <summary>
+        /// gets check
+        /// </summary>
+        public Boolean StartupIsChecked
+        {
+            get
+            {
+                bool run = EK_App.Settings1.Default.RunAtStartup;
+                if (run)
+                {
+                    Enable_Startup();
+                }
+                else
+                {
+                    Disable_Startup();
+                } 
+
+                return run;
+            }
+            set
+            {
+                EK_App.Settings1.Default.RunAtStartup = value;
+                EK_App.Settings1.Default.Save();
+                OnPropertyChanged("StartupIsChecked"); 
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this,
+                    new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+        #endregion 
     }
 
 
